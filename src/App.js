4eca46a06330f87a36ae2693f02e95fd97087864
CDNJ <contact.cdnjmusic@gmail.com>
2023-09-15 00:27:56 +0530

    Update logo and link consts

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import './App.css';
 
 function App() {
+  const logoUrl = 'https://www.propelleraero.com/wp-content/uploads/2021/05/Vector.svg';
+  const linkUrl = 'https://www.propelleraero.com/dirtmate/';
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={`https://www.propelleraero.com/wp-content/uploads/2021/05/Vector.svg`} className="App-logo" alt="logo" />
+        <a href={linkUrl} target="_blank" rel="noopener noreferrer">
+          <img src={logoUrl} className="App-logo" alt="logo" />
+        </a>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
-          href="https://www.propelleraero.com/dirtmate/"
+          href={linkUrl}
           target="_blank"
           rel="noopener noreferrer"
         >
